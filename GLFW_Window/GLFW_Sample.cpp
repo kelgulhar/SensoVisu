227fa3bd76fe15e30db682092a323178feada6a8
Stefan Zuza <stefan.zuza@yahoo.de>
2023-01-29 15:19:54 +0100

    modified GLFW_Window; added GUI src code

--- GLFW_Window/GLFW_Sample.cpp
+++ GLFW_Window/GLFW_Sample.cpp
@@ -1,47 +1,32 @@
 #include <librealsense2/rs.hpp>
 #include <iostream>
-
-
-
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <Windows.h>
 
 
-int main(void)
-{
-	GLFWwindow* window;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+//test code for creation of multiple windows. Those need to be represented in Dear ImGui while handling context -> camera/frame
+//this code only generates two windows. the whole programm terminates if any of the two windows is closed
+int main() {
+	glfwInit();
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "SensoVisu_CAM_X", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+	GLFWwindow* window1 = glfwCreateWindow(800, 600, "Cam#1", NULL, NULL);
+	glfwMakeContextCurrent(window1);
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+	GLFWwindow* window2 = glfwCreateWindow(640, 480, "Cam#2", NULL, NULL);
+	glfwMakeContextCurrent(window2);
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+	while (!glfwWindowShouldClose(window1) && !glfwWindowShouldClose(window2)) {
+		// Render to first window
+		glfwMakeContextCurrent(window1);
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+		// Render to second window
+		glfwMakeContextCurrent(window2);
 
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
-	
-	Sleep(10000);
 	glfwTerminate();
 	return 0;
 }
